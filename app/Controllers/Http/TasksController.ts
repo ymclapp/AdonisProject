350660900db0ef3a9ccbe57fb096512706e708d9
ymcla <ymclapp@yahoo.com>
2021-12-09 12:07:48 -0600

    added a validator and tried to get the validate to work.  index is no longer working

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -1,5 +1,6 @@
 //import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Task from 'App/Models/Task'
+import TaskValidator from 'App/validators/Task'
 
 export default class TasksController {
   public async index({ view }) {
@@ -7,5 +8,16 @@ export default class TasksController {
 
     return view.render('tasks.index', { tasks })
   }
+
+  public async store({ request, session, response }) {
+    const data = await request.validate(TaskValidator)
+
+    await Task.create(data)
+
+    session.flash({ notification: 'Task added!' })
+
+    return response.redirect('back')
+  }
 }
+
 
