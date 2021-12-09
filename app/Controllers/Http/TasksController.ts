f9a107e35297be5caa54f2ed8a610802bba87101
ymcla <ymclapp@yahoo.com>
2021-12-09 10:24:08 -0600

    updated inside taskscontroller the export of taskscontroller vs taskcontroller. getting red squigglies on import of Task, but it is building and showing tasks

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -1,7 +1,7 @@
 //import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Task from 'App/Models/Task'
 
-export default class TaskController {
+export default class TasksController {
   public async index({ view }) {
     const tasks = await Task.all()
 
