5fb796b04b2fe3db09f2585663152b4c6c666619
ymcla <ymclapp@yahoo.com>
2021-12-09 09:55:01 -0600

    updated the controller to hopefully get it to build and show tasks

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -1,12 +1,20 @@
 //import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Task from 'App/Models/Task'
 
-// remember to reference the Task model at the top
-const Task = use('App/Models/Task')
+export default class TaskController {
+  public async index({ view }) {
+    const tasks = await Task.all()
 
-async index({ view }) {
-  const tasks = await Task.all()
-
-  return view.render('tasks.index', { tasks: tasks.toJSON() })
+    return view.render('tasks.index', { tasks })
+  }
 }
+// remember to reference the Task model at the top
+//const Task = use('App/Models/Task')
 
+//class TaskController {
 
+//async index({ view }) {
+// const tasks = await Task.all()
+
+//  return view.render('tasks.index', { tasks: tasks.toJSON() })
+//}
