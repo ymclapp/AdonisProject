3bde5e7ec20d30fe1150f8a8718b5770a8cc58c8
ymcla <ymclapp@yahoo.com>
2021-12-10 10:29:26 -0600

    activated the welcome page again and updated

--- start/routes.ts
+++ start/routes.ts
@@ -20,10 +20,10 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-//Route.get('/', async ({ view }) => {
-//  return view.render('welcome')
-//})
-Route.get('/', 'TasksController.index')
+Route.get('/welcome', async ({ view }) => {
+  return view.render('welcome')
+})
+Route.get('/tasks', 'TasksController.index')
 Route.post('tasks', 'TasksController.store')
 Route.patch('tasks/:id', 'TasksController.update')
 Route.delete('tasks/:id', 'TasksController.destroy')
